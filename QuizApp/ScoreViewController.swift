32efea7a75e2069df53d32414be62db0873ff9fe
awa228 <yuuhei228@hotmail.co.jp>
2021-01-17 00:41:07 +0900

    スコア画面に値を渡し、スコアを表示させるようにした

--- QuizApp/ScoreViewController.swift
+++ QuizApp/ScoreViewController.swift
@@ -8,22 +8,23 @@
 import UIKit
 
 class ScoreViewController: UIViewController {
+    
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    //ScoreViewController.swiftから、スコアの値を受け取る箱
+    var correct = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        scoreLabel.text = "\(correct)問正解！！"
+     
+    }
+    //二つ前の画面へ戻る処理
+    @IBAction func toTouButtonAction(_ sender: Any) {
+        self.presentingViewController?.presentingViewController?.dismiss(animated: true)
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+  
 }
