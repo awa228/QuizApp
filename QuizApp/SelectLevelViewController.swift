34632ecbb6391884f49de6902062c554c352b864
awa228 <yuuhei228@hotmail.co.jp>
2021-02-05 16:52:44 +0900

    Interstitialでつまずいた

---
+++ QuizApp/SelectLevelViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SelectLevelViewController.swift
+//  QuizApp
+//
+//  Created by 大嶋祐平 on 2021/02/04.
+//
+
+import UIKit
+
+class SelectLevelViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
